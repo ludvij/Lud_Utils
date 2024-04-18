cb00be15c7b28da3f4f8842ce9424a1dadf1f3fa
ludvij <vijande48@gmail.com>
2024-04-18 07:11:17 +0200

    added file writing functionality to john slurper

--- lud_slurper.hpp
+++ lud_slurper.hpp
@@ -2,25 +2,29 @@
 #define LUD_SLURPER_HEADER
 
 #include <ostream>
-#include <string_view>
 #include <string>
+#include <string_view>
 
-#include <fstream>
+#include <algorithm>
+#include <numeric>
+
 #include <filesystem>
+#include <fstream>
 
-#include <vector>
 #include <bit>
+#include <span>
+#include <vector>
 
+#include <format>
 
+namespace Lud
+{
 
-namespace Lud {
-
-class Slurper 
+class Slurper
 {
 public:
-	Slurper();
-	Slurper(const std::string_view& filename, std::ios_base::openmode mode = std::ios_base::in);
-	Slurper(const std::string& filename, std::ios_base::openmode mode = std::ios_base::in);
+	Slurper() = default;
+	Slurper(const std::string_view filename, std::ios_base::openmode mode = std::ios_base::in);
 	Slurper(const char* filename, std::ios_base::openmode mode = std::ios_base::in);
 
 	Slurper(const Slurper& slurper) = delete;
@@ -38,10 +42,11 @@ public:
 	// reads size of structure from file to provided structure
 	template<typename T> Slurper& ReadChunk(T& t);
 	Slurper& Read(char* data, size_t size);
+	template<typename T> T Read();
 
 
 	// returns current pos in file
-	size_t Where();
+	std::streampos Where();
 	// moves current file pos to provided pos
 	Slurper& Move(std::streamoff pos);
 	// moves current file pos by an offset in a direction
@@ -54,21 +59,43 @@ public:
 	bool IsOpen() const;
 
 	// opens file 
-	Slurper& Open(const std::string_view& filename, std::ios_base::openmode mode = std::ios_base::in);
-	Slurper& Open(const std::string& filename, std::ios_base::openmode mode = std::ios_base::in);
-	Slurper& Open(const char* filename, std::ios_base::openmode mode = std::ios_base::in);
+	bool Open(const std::string_view filename, std::ios_base::openmode mode = std::ios_base::in);
+	bool Open(const char* filename, std::ios_base::openmode mode = std::ios_base::in);
 
 	// returns file as vector of lines without you needing to creating a slurper
-	static std::vector<std::string> Slurp(const std::string_view& filename, std::ios_base::openmode mode = std::ios_base::in);
-	static std::vector<std::string> Slurp(const std::string& filename, std::ios_base::openmode mode = std::ios_base::in);
+	static std::vector<std::string> Slurp(const std::string_view filename, std::ios_base::openmode mode = std::ios_base::in);
 	static std::vector<std::string> Slurp(const char* filename, std::ios_base::openmode mode = std::ios_base::in);
 
 	// reads whole file to T vector
 	template<typename T> std::vector<T> ReadTo();
 
-	template<typename T> static std::vector<T> SlurpTo(const std::string_view& filename, std::ios_base::openmode mode = std::ios_base::in);
-	template<typename T> static std::vector<T> SlurpTo(const std::string& filename, std::ios_base::openmode mode = std::ios_base::in);
+	template<typename T> static std::vector<T> SlurpTo(const std::string_view filename, std::ios_base::openmode mode = std::ios_base::in);
 	template<typename T> static std::vector<T> SlurpTo(const char* filename, std::ios_base::openmode mode = std::ios_base::in);
+
+	// write funcionality
+	template<typename T> Slurper& WriteList(const std::span<T> list, const std::string_view delim, std::optional<size_t> chunk_size = std::nullopt);
+	template<typename T> Slurper& WriteList(const std::span<T> list, const char* delim, std::optional<size_t> chunk_size = std::nullopt);
+
+	Slurper& Write(const char* n, std::streampos s);
+	template<typename T> Slurper& Write(T dat);
+	template<class... Args> Slurper& WriteFmt(const std::format_string<Args...> fmt, Args... args);
+	template<typename T> Slurper& WriteListFmt(const std::span<T> list, const std::format_string<T> fmt, const std::string_view delim, std::optional<size_t> chunk_size = std::nullopt);
+	template<typename T> Slurper& WriteListFmt(const std::span<T> list, const std::format_string<T> fmt, const char* delim, std::optional<size_t> chunk_size = std::nullopt);
+
+	Slurper& WriteLine(const std::string_view line = "");
+	Slurper& WriteLine(const char* line = "");
+
+	template<class... Args> Slurper& WriteFmtLine(const std::format_string<Args...> fmt, Args... args);
+
+	Slurper& WriteLines(const std::span<std::string_view> lines);
+	Slurper& WriteLines(const std::span<const char*> lines);
+
+	template<typename T> std::ostream& operator <<(const T& t);
+	template<typename T> std::istream& operator >>(T& t);
+
+	Slurper& SetTabLevel(int level);
+
+	Slurper& NewLine();
 
 	// closes file
 	void Close();
@@ -76,38 +103,29 @@ public:
 	bool HasSpace(size_t size);
 
 private:
-	std::ifstream m_file;
+	std::fstream m_file;
 	std::ios_base::openmode m_mode{};
+
+	int m_tab_level = 0;
+	std::string m_tabs;
 
 };
 }
 
 #ifdef LUD_SLURPER_IMPLEMENTATION
-namespace Lud {
-
-
-inline Slurper::Slurper()
+namespace Lud
 {
-
-}
-
-inline Slurper::Slurper(const std::string_view& filename, const std::ios_base::openmode mode) 
+inline Slurper::Slurper(const std::string_view filename, const std::ios_base::openmode mode)
 	: Slurper()
-{ 
+{
 	m_mode = mode;
-	Open(filename, mode); 
+	Open(filename, mode);
 }
-inline Slurper::Slurper(const std::string& filename, const std::ios_base::openmode mode) 
+inline Slurper::Slurper(const char* filename, const std::ios_base::openmode mode)
 	: Slurper()
-{ 
+{
 	m_mode = mode;
-	Open(filename, mode); 
-}
-inline Slurper::Slurper(const char* filename, const std::ios_base::openmode mode ) 
-	: Slurper()
-{ 
-	m_mode = mode;
-	Open(filename, mode); 
+	Open(filename, mode);
 }
 
 inline Slurper::~Slurper()
@@ -115,11 +133,6 @@ inline Slurper::~Slurper()
 	Close();
 }
 
-inline std::vector<std::string> Slurper::Slurp(const std::string& filename, const std::ios_base::openmode mode)
-{
-	Slurper file(filename, mode);
-	return file.ReadLines();
-}
 
 inline std::vector<std::string> Slurper::Slurp(const char* filename, const std::ios_base::openmode mode)
 {
@@ -127,35 +140,40 @@ inline std::vector<std::string> Slurper::Slurp(const char* filename, const std::
 	return file.ReadLines();
 }
 
-inline std::vector<std::string> Slurper::Slurp(const std::string_view& filename, const std::ios_base::openmode mode)
+inline std::vector<std::string> Slurper::Slurp(const std::string_view filename, const std::ios_base::openmode mode)
 {
 	Slurper file(filename, mode);
 	return file.ReadLines();
 }
 
-inline Slurper& Lud::Slurper::Open(const std::string_view& filename, const std::ios_base::openmode mode) 
+inline bool Lud::Slurper::Open(const std::string_view filename, const std::ios_base::openmode mode)
 {
-	if (IsOpen()) {
+	if (IsOpen())
+	{
 		Close();
 	}
+	if (!( mode & std::ios::in ) && !( mode & std::ios::out ))
+	{
+		throw std::runtime_error("Must use in or out mode");
+	}
+
 	m_file.open(std::string(filename), mode);
-	return *this;
+	return IsOpen();
+
 }
-inline Slurper& Lud::Slurper::Open(const std::string& filename, const std::ios_base::openmode mode) 
-{ 
-	if (IsOpen()) {
+inline bool Lud::Slurper::Open(const char* filename, const std::ios_base::openmode mode)
+{
+	if (IsOpen())
+	{
 		Close();
 	}
-	m_file.open(filename, mode);
-	return *this;
-} 
-inline Slurper& Lud::Slurper::Open(const char* filename, const std::ios_base::openmode mode) 
-{ 
-	if (IsOpen()) {
-		Close();
+	if (!( mode & std::ios::in ) && !( mode & std::ios::out ))
+	{
+		throw std::runtime_error("Must use in or out mode");
 	}
 	m_file.open(filename, mode);
-	return *this;
+	return IsOpen();
+
 }
 
 inline bool Slurper::IsOpen() const
@@ -163,7 +181,7 @@ inline bool Slurper::IsOpen() const
 	return m_file.is_open();
 }
 
-inline Slurper& Slurper::Reset() 
+inline Slurper& Slurper::Reset()
 {
 	m_file.clear();
 	m_file.seekg(0, std::ios::beg);
@@ -171,6 +189,59 @@ inline Slurper& Slurper::Reset()
 	return *this;
 }
 
+inline Slurper& Slurper::Write(const char* n, std::streampos s)
+{
+	m_file.write(n, s);
+	return *this;
+}
+
+inline Slurper& Slurper::WriteLine(const std::string_view line)
+{
+	m_file << line;
+	NewLine();
+	return *this;
+}
+
+inline Slurper& Slurper::WriteLine(const char* line)
+{
+	m_file << line;
+	NewLine();
+
+	return *this;
+}
+
+inline Slurper& Slurper::WriteLines(const std::span<std::string_view> lines)
+{
+	for (const auto& line : lines)
+	{
+		WriteLine(line);
+	}
+
+	return *this;
+}
+
+inline Slurper& Slurper::WriteLines(const std::span<const char*> lines)
+{
+	for (const auto& line : lines)
+	{
+		WriteLine(line);
+	}
+
+	return *this;
+}
+
+inline Slurper& Slurper::SetTabLevel(int level)
+{
+	m_tab_level = level;
+	m_tabs = std::string(m_tab_level, '\t');
+	return *this;
+}
+
+inline Slurper& Slurper::NewLine()
+{
+	m_file << '\n' << m_tabs;
+	return *this;
+}
 
 inline void Slurper::Close()
 {
@@ -179,9 +250,17 @@ inline void Slurper::Close()
 
 template<class T> Slurper& Slurper::ReadChunk(T& t)
 {
-	Read(std::bit_cast<char*>(&t), sizeof(t));
+	Read(std::bit_cast<char*>( &t ), sizeof(t));
 
 	return *this;
+}
+
+template <typename T>
+inline T Slurper::Read()
+{
+	T dat;
+	m_file >> dat;
+	return dat;
 }
 
 inline std::string Slurper::ReadLine()
@@ -196,31 +275,42 @@ inline std::vector<std::string> Slurper::ReadLines()
 {
 	std::vector<std::string> lines;
 
-	for(std::string line; std::getline(m_file, line);) {
+	for (std::string line; std::getline(m_file, line);)
+	{
 		lines.emplace_back(line);
 	}
 	return lines;
 }
 
-inline Slurper &Slurper::Read(char *data, size_t size)
+inline Slurper& Slurper::Read(char* data, size_t size)
 {
 	m_file.read(data, size);
+	return *this;
 }
 
-inline size_t Slurper::Where()
+inline std::streampos Slurper::Where()
 {
-	return m_file.tellg();
+	if (m_mode & std::ios_base::in)
+		return m_file.tellg();
+	else
+		return m_file.tellp();
 }
 
 inline Slurper& Slurper::Move(const std::streamoff pos)
 {
-	m_file.seekg(pos);
+	if (m_mode & std::ios_base::in)
+		m_file.seekg(pos);
+	else
+		m_file.seekp(pos);
 	return *this;
 }
 
 inline Slurper& Slurper::Move(const std::streamoff offset, const std::ios_base::seekdir dir)
 {
-	m_file.seekg(offset, dir);
+	if (m_mode & std::ios_base::in)
+		m_file.seekg(offset, dir);
+	else
+		m_file.seekp(offset, dir);
 	return *this;
 }
 
@@ -237,24 +327,17 @@ inline bool Slurper::HasSpace(const size_t size)
 template<typename T>
 inline std::vector<T> Slurper::ReadTo()
 {
-	if (!(m_mode & std::ios::ate))
+	if (!( m_mode & std::ios::ate ))
 		Move(0, std::ios::end);
-	const size_t file_size = static_cast<size_t>(Where());
+	const size_t file_size = static_cast<size_t>( Where() );
 	std::vector<T> buffer(file_size / sizeof(T));
 	Move(0);
-	m_file.read(std::bit_cast<char*>(buffer.data()), file_size);
-	
+	m_file.read(std::bit_cast<char*>( buffer.data() ), file_size);
+
 	return buffer;
 }
 template<typename T>
-inline std::vector<T> Slurper::SlurpTo(const std::string_view& filename, std::ios_base::openmode mode)
-{
-	Slurper file(filename, mode | std::ios::ate);
-	return file.ReadTo<T>();
-}
-
-template<typename T>
-inline std::vector<T> Slurper::SlurpTo(const std::string& filename, std::ios_base::openmode mode)
+inline std::vector<T> Slurper::SlurpTo(const std::string_view filename, std::ios_base::openmode mode)
 {
 	Slurper file(filename, mode | std::ios::ate);
 	return file.ReadTo<T>();
@@ -267,6 +350,109 @@ inline std::vector<T> Slurper::SlurpTo(const char* filename, std::ios_base::open
 	return file.ReadTo<T>();
 }
 
+template <typename T>
+inline Slurper& Slurper::WriteList(const std::span<T> list, const std::string_view delim, std::optional<size_t> chunk_size)
+{
+	for (size_t i = 0; i < list.size(); i++)
+	{
+		if (chunk_size && i % *chunk_size == 0)
+		{
+			NewLine();
+		}
+		m_file << list[i];
+		if (i != list.size() - 1)
+		{
+			m_file << delim;
+		}
+	}
+
+	return *this;
+}
+
+template<typename T>
+inline Slurper& Slurper::WriteList(const std::span<T> list, const char* delim, std::optional<size_t> chunk_size)
+{
+	for (size_t i = 0; i < list.size(); i++)
+	{
+		if (chunk_size && i % *chunk_size == 0)
+		{
+			NewLine();
+		}
+		m_file << list[i];
+		if (i != list.size() - 1)
+		{
+			m_file << delim;
+		}
+	}
+	return *this;
+}
+
+template <typename T>
+inline Slurper& Slurper::Write(T dat)
+{
+	m_file << dat;
+	return *this;
+}
+template<class ...Args>
+inline Slurper& Slurper::WriteFmt(const std::format_string<Args...> fmt, Args ...args)
+{
+	m_file << std::format(fmt, std::forward<Args>(args)...);
+	return *this;
+}
+template<typename T>
+inline Slurper& Slurper::WriteListFmt(const std::span<T> list, const std::format_string<T> fmt, const std::string_view delim, std::optional<size_t> chunk_size)
+{
+	for (size_t i = 0; i < list.size(); i++)
+	{
+		if (chunk_size && i % *chunk_size == 0)
+		{
+			NewLine();
+		}
+		m_file << std::format(fmt, std::forward<T>(list[i]));
+		if (i != list.size() - 1)
+		{
+			m_file << delim;
+		}
+	}
+	return *this;
+}
+template<typename T>
+inline Slurper& Slurper::WriteListFmt(const std::span<T> list, const std::format_string<T> fmt, const char* delim, std::optional<size_t> chunk_size)
+{
+	for (size_t i = 0; i < list.size(); i++)
+	{
+		if (chunk_size && i % *chunk_size == 0)
+		{
+			NewLine();
+		}
+		m_file << std::format(fmt, std::forward<T>(list[i]));
+		if (i != list.size() - 1)
+		{
+			m_file << delim;
+		}
+	}
+	return *this;
+}
+template<class ...Args>
+inline Slurper& Slurper::WriteFmtLine(const std::format_string<Args...> fmt, Args ...args)
+{
+	m_file << std::format(fmt, std::forward<Args>(args)...);
+	NewLine();
+
+	return *this;
+}
+template<typename T>
+inline std::ostream& Slurper::operator<<(const T& t)
+{
+	m_file << t;
+	return m_file;
+}
+template<typename T>
+inline std::istream& Slurper::operator>>(T& t)
+{
+	m_file >> t;
+	return m_file;
+}
 }
 
 #endif
