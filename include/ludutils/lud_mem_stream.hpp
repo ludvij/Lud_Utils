8c1b73ee63e1c56ef8ebccf123b298b4b70ca666
ludvij <vijande48@gmail.com>
2025-10-24 00:10:22 +0200

    modified default mem_streambuf to take in span, started work in vector-wrapping buffer

--- include/ludutils/lud_mem_stream.hpp
+++ include/ludutils/lud_mem_stream.hpp
@@ -5,6 +5,9 @@
 
 #include <iostream>
 #include <streambuf>
+#include <cstdint>
+#include <vector>
+#include <span>
 
 namespace Lud
 {
@@ -25,15 +28,16 @@ concept BinaryRange = requires
 	requires std::same_as<std::ranges::range_value_t<R>, C>;
 };
 
-
+// C++ standard does not provide char_trais spect for
+// uint8_t and recommends just casting when using binary data
 template<ByteType T = char>
-class memory_streambuf : public std::streambuf
+class view_streambuf : public std::streambuf
 {
 public:
-	memory_streambuf(T* ptr, size_t size);
-	memory_streambuf() = default;
+	view_streambuf(std::span<T> data);
+	view_streambuf() = default;
 
-	void Link(T* ptr, size_t size);
+	void Link(std::span<T> data);
 
 protected:
 	virtual pos_type seekoff(off_type off, std::ios_base::seekdir dir, std::ios_base::openmode which = std::ios_base::in) override;
@@ -41,18 +45,31 @@ protected:
 };
 
 template<ByteType T = char>
+class vector_wrap_streambuf : public std::streambuf
+{
+public:
+	vector_wrap_streambuf(std::vector<T>& buffer);
+
+	
+protected:
+private:
+	std::vector<T>& m_buffer;
+};
+
+template<ByteType T = char>
 class memory_istream : public std::istream
 {
 public:
-	memory_istream(T* s, size_t n);
+	memory_istream(std::span<T> data);
 
 	memory_istream() = default;
 
-	void Link(T* s, size_t n);
+	void Link(std::span<T> data);
 	
 private:
-	memory_streambuf<T> m_buffer;
+	view_streambuf<T> m_buffer;
 };
+
 
 
 }
@@ -64,44 +81,62 @@ private:
 #define LUD_WRITE_BINARY(stream, var) LUD_WRITE_BINARY_PTR(stream, &var, sizeof var)
 
 template<Lud::ByteType T>
-Lud::memory_streambuf<T>::memory_streambuf(T* s, size_t n)
+Lud::view_streambuf<T>::view_streambuf(std::span<T> data)
 {
-	Link(s, n);
+	Link(data);
 }
 
 template<Lud::ByteType T>
-Lud::memory_streambuf<T>::pos_type Lud::memory_streambuf<T>::seekoff(off_type off, std::ios_base::seekdir dir, [[maybe_unused]] std::ios_base::openmode which)
+Lud::view_streambuf<T>::pos_type Lud::view_streambuf<T>::seekoff(off_type off, std::ios_base::seekdir dir, std::ios_base::openmode which)
 {
+	if (which & std::ios_base::out)
+	{
+		throw std::runtime_error("Writing is currently not supported!");
+	}
+
 	if (dir == std::ios_base::cur)
+	{
 		gbump(static_cast<int>( off ));
+	}
 	else if (dir == std::ios_base::end)
+	{
 		setg(eback(), egptr() + off, egptr());
+	}
 	else if (dir == std::ios_base::beg)
+	{
 		setg(eback(), eback() + off, egptr());
+	}
+	
 	return gptr() - eback();
 }
 
 
 template<Lud::ByteType T>
-Lud::memory_streambuf<T>::pos_type Lud::memory_streambuf<T>::seekpos(pos_type pos, [[maybe_unused]] std::ios_base::openmode which)
+Lud::view_streambuf<T>::pos_type Lud::view_streambuf<T>::seekpos(pos_type pos, std::ios_base::openmode which)
 {
+	if (which & std::ios_base::out)
+	{
+		throw std::runtime_error("Writing is currently not supported!");
+	}
+
 	setg(eback(), eback() + pos, egptr());
 	return gptr() - eback();
 }
 
 
+
 template <Lud::ByteType T>
-void Lud::memory_streambuf<T>::Link(T *ptr, size_t size)
+void Lud::view_streambuf<T>::Link(std::span<T> data)
 {
-	char* cs = std::bit_cast<char*>(ptr);
-	setg(cs, cs, cs + size);
+	char* cs = std::bit_cast<char*>(data.data());
+	setg(cs, cs, cs + data.size());
 }
 
 
 template<Lud::ByteType T>
-Lud::memory_istream<T>::memory_istream(T* s, size_t n)
+Lud::memory_istream<T>::memory_istream(std::span<T> data)
 	: std::istream(&m_buffer)
-	, m_buffer(s, n)
+	, m_buffer(data)
 {
 	rdbuf(&m_buffer);
 }
@@ -109,9 +144,20 @@ Lud::memory_istream<T>::memory_istream(T* s, size_t n)
 
 
 template <Lud::ByteType T>
-void Lud::memory_istream<T>::Link(T *s, size_t n)
+void Lud::memory_istream<T>::Link(std::span<T> data)
 {
-	m_buffer.Link(s, n);
+	m_buffer.Link(data);
+}
+
+
+
+template <Lud::ByteType T>
+Lud::vector_wrap_streambuf<T>::vector_wrap_streambuf(std::vector<T> &buffer)
+	: m_buffer(buffer)
+{
+	const char* cs = std::bit_cast<char*>(m_buffer.data());
+	setg(cs, cs, cs + m_buffer.size());
+	setp(cs, cs + m_buffer.size());
 }
 
 #endif//!LUD_MEMORY_STREAM_HEADER
